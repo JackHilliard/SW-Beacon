8876a777b2020517f3b0536dd03d57add1d8f9a6
JackHilliard <47607167+JackHilliard@users.noreply.github.com>
2019-09-13 17:56:48 +0100

    Update queries.py

--- lib/queries.py
+++ lib/queries.py
@@ -1,7 +1,7 @@
 sql = {
     "getNewReadings": 'SELECT br.id_Readings, b.mac, br.rssi, br.timestamp FROM Beacons AS b JOIN Beacon_Readings AS br ON (br.mac_FK = b.id AND br.state = "NEW") LIMIT 400',
     "retryReadings": 'SELECT br.id_Readings, b.mac, br.rssi, br.timestamp FROM Beacons AS b JOIN Beacon_Readings AS br ON (br.mac_FK = b.id AND br.state = "FAILED") LIMIT 400',
-    "getBeacons": 'SELECT id, mac FROM Beacons',
+    "getBeacons": 'SELECT id, mac FROM Beacons WHERE active = "TRUE"',
     "deleteReading": 'DELETE FROM Beacon_Readings WHERE id_Readings=?',
     "insertBeacon": 'INSERT INTO Beacons (mac) VALUES (?)',
     "deleteBeacon": 'DELETE FROM Beacons WHERE mac=?',
