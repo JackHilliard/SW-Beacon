49c8ec1a7d2536c641128ae9db11bdba16cb47bf
JackHilliard <47607167+JackHilliard@users.noreply.github.com>
2019-09-18 11:05:07 +0100

    Average scan
    + scans for scanInterval then averages readings for each beacon

--- scanner/scanAndStore_Rssi.py
+++ scanner/scanAndStore_Rssi.py
@@ -13,6 +13,13 @@ import signal
 3. Save readings only for mac addresses that are in the beacon table 
 '''
 
+class BLEBeacon:
+    def __init__(self, macAddr):
+        self.macAddr = macAddr
+        self.combRssi = 0
+        self.countRssi = 0
+        self.avgRssi = 0
+
 def macsOnly(beaconRow):
     return beaconRow['mac']
 
@@ -21,17 +28,28 @@ def save_data(connection):
     cur = connection.cursor()
     cur.execute(sql['getBeacons'])
     db_beacons = cur.fetchall()
+    beacons = []
+    for db_beacon in range(db_beacons)):
+        beacons.append(BLEBeacon(db_beacon['mac']))
     print('Scanning LE devices (' + str(conf['scanInterval']) + 's)')
     discovered = scanner.scan(timeout=conf['scanInterval'])
     print('Finished scanning. Saw: ' + str([dev.addr.upper() for dev in discovered if dev.addr.upper() in map(macsOnly, db_beacons)]))
     for device in discovered:
         dev_mac = device.addr.upper()
         dev_rssi = device.rssi
-        beacons = [db_beacon for db_beacon in db_beacons if db_beacon['mac'] == dev_mac]
-        if (len(beacons) == 1):
+        for beacon in beacons:
+            if dev_mac == beacon.macAddr:
+                beacon.combRssi+=dev_rssi
+                beacon.countRssi+=1
+                break
+        
+        #beacons = [db_beacon for db_beacon in db_beacons if db_beacon['mac'] == dev_mac]
+    for beacon in beacons:
+        if beacon.countRssi != 0:
+            beacon.avgRssi = beacon.combRssi/beacon.countRssi
             now = int(round(time.time() * 1000))
             cur = connection.cursor()
-            cur.execute(sql['insertReading'], (dev_rssi, now, 'NEW', beacons[0]['id']))
+            cur.execute(sql['insertReading'], (beacon.avgRssi, now, 'NEW', beacon.macAddr))
 
     try:
         connection.commit()
