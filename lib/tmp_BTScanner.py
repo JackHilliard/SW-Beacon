8b114e6decc2db6c07c78ee83f6937d7121aec5e
JackHilliard <47607167+JackHilliard@users.noreply.github.com>
2019-10-09 17:32:37 +0100

    Create tmp_BTScanner.py
    new bluetooth scanner that just uses bluez by turning on the hcitool and then reading from the hcidump

---
+++ lib/tmp_BTScanner.py
@@ -0,0 +1,47 @@
+import re
+import sys
+import os
+import subprocess
+
+class BLEScanner:
+
+    hcitool = None
+    hcidump = None
+    
+    def start(self):
+        print('Start receiving broadcasts')
+        DEVNULL = subprocess.DEVNULL if sys.version_info > (3, 0) else open(os.devnull, 'wb')
+
+        subprocess.call('sudo hciconfig hci0 reset', shell = True, stdout = DEVNULL)
+        self.hcitool = subprocess.Popen(['sudo', '-n', 'hcitool', 'lescan', '--duplicates'], stdout = DEVNULL)
+        self.hcidump = subprocess.Popen(['sudo', '-n', 'hcidump', '--raw'], stdout=subprocess.PIPE)
+
+    def stop(self):
+        print('Stop receiving broadcasts')
+        subprocess.call(['sudo', 'kill', str(self.hcidump.pid), '-s', 'SIGINT'])
+        subprocess.call(['sudo', '-n', 'kill', str(self.hcitool.pid), '-s', "SIGINT"])
+
+    def get_lines(self):
+        data = None
+        try:
+            print("reading hcidump...\n")
+            #for line in hcidump.stdout:
+            while True:
+                line = self.hcidump.stdout.readline()
+                line = line.decode()
+                if line.startswith('> '):
+                    yield data
+                    data = line[2:].strip().replace(' ', '')
+                elif line.startswith('< '):
+                    data = None
+                else:
+                    if data:
+                        data += line.strip().replace(' ', '')
+        except KeyboardInterrupt as ex:
+            print("kbi")
+            scanner.stop()
+            exit(0)
+            return
+        except Exception as ex:
+            print(ex)
+            return
